5718e309a4117a7d117cf2c5c3d530fb87bdeb0c
jorge <jorgeabaron87@gmail.com>
2014-08-01 11:19:54 +0200

    Se han quitado todas las clases y archivos que no se usaban al incluir la integracion de JPA con Hibernate y Spring.

--- ServiceWeb/src/main/java/com/spring/controller/Greeter.java
+++
@@ -1,7 +0,0 @@
-package com.spring.controller;
-
-public class Greeter {
-	public String sayHello() {
-        return "Hello world!";
-    }
-}
--- ServiceWeb/src/main/java/com/spring/model/CategoriaDAOImpl.java
+++ ServiceWeb/src/main/java/com/spring/model/CategoriaDAOImpl.java
@@ -12,23 +12,6 @@ import com.spring.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaDAOImpl implements CategoriaDAO{
-
-	/*private SessionFactory sessionFactory;
-	 
-    public CategoriaDAOImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-	
-    @Override
-    @Transactional
-    public List<Categoria> list() {
-        @SuppressWarnings("unchecked")
-        List<Categoria> listCategoria = (List<Categoria>) sessionFactory.getCurrentSession()
-                .createCriteria(Categoria.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
- 
-        return listCategoria;
-    }*/
 	
 	@Resource
 	private CategoriaRepository categoriaRepository;
